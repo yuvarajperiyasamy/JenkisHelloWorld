c0f1ca229f0dd606cb5025170b0f38df6bb4d0e1
yuvarajperiyasamy <75745059+yuvarajperiyasamy@users.noreply.github.com>
2020-12-16 04:34:41 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("-----*****Need some changes*****-----");
+		System.out.println("-----*****Trying Webhook*****-----");
 	}
 }
